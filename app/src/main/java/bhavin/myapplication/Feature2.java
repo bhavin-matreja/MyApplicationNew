0c54e1b2eb6b5e7e55b93cdfb2c6544cd9f16821
bhavin <gr8.person.me@gmail.com>
2019-01-16 12:39:41 +0530

    changes in Feature2 Class

--- app/src/main/java/bhavin/myapplication/Feature2.java
+++ app/src/main/java/bhavin/myapplication/Feature2.java
@@ -1,4 +1,14 @@
 package bhavin.myapplication;
 
-public class Feature2 {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Feature2 extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
